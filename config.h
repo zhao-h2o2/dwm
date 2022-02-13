86be61d0c2bdffafc515a5fdc8b7aecee4d1deff
zhaohui wang <18811365389@163.com>
2022-02-13 14:10:34 +0800

    remove keymod

--- config.h
+++ config.h
@@ -259,7 +259,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_comma,      focusmon,               {.i = -1 } },
 	{ MODKEY,                       XK_period,     focusmon,               {.i = +1 } },
 
-	{ MODKEY,                       XK_Escape,     setkeymode,             {.ui = COMMANDMODE} },
 	{ MODKEY,                       XK_i,          incnmaster,             {.i = +1 } },
 	{ MODKEY,                       XK_d,          incnmaster,             {.i = -1 } },
 	{ MODKEY,                       XK_h,          setmfact,               {.f = -0.05} },
